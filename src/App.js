7c113f5adda843b9527278dd4a8a0ad08c26f71a
sanghami.d <sanghami.d@gavstech.com>
2023-10-30 14:16:36 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {useState} from 'react';
+function App(props) {
+ // console.log(this.process.argv[0]);
+  //console.log(this.process.argv[1]);
+ // console.log(this.process.aegv[2]);
+ // console.log(this.process.argv.length + 'parameters' ); 
+  var username = prompt("Enter your name");
+  if(props.name == username){
+    var today = new Date();
+    if(today.getHours() >= Number(props.visitingTime)){
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <b>Welcome Mr/Mrs</b> {props.firstName} &nbsp; &nbsp; {props.lastName}
+      <input type='text' minLength="10" maxLength="20" ></input>
     </div>
-  );
+  );}
+  else{
+    return(<div className='App'><b>Visit after {props.visitingTime} AM in your local time</b></div>)
+  }
+}
+else {
+  return(<div className='App'><b>{props.username}Your account is blocked. Please contact administration.</b></div>)
+}
 }
 
 export default App;
